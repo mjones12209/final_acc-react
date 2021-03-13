27998b6f4ab3ed7a067ae80fe26c77171b5d7d73
mjones12209 <m.jones12209@gmail.com>
2021-03-13 16:53:53 -0500

    added state to Genre.js and useEffect

--- src/components/Genre/Genre.js
+++ src/components/Genre/Genre.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { Dropdown } from 'react-bootstrap';
 import axios from 'axios';
 import {apiKey} from '../../key';
@@ -7,68 +7,67 @@ import PropTypes from 'prop-types';
 
 const Genre = (props) => {
 
-  const genres = [];
+  const [dropdowns, setDropdowns] = useState();
 
   useEffect(()=> {
-    const axiosCall = async (genreId, genreName) => {
+    (async function () {
       try {
         const options = {
           method: "GET",
-          url: `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=${genreId}&include_adult=false&include_video=false&page=1`,
+          url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`,
         };
         const asyncResponse = await axios(options);
-        props.setGenre(genreName);
-        props.setMovies(
-          asyncResponse.data.results.map((movie) => {
+
+        setDropdowns(
+          asyncResponse.data.genres.map((genre) => {
             return (
-              <MoviesDisplay
-                picture={movie.backdrop_path}
-                title={movie.original_title}
-                desc={movie.overview}
-                key={movie.id}
-              />
+              <Dropdown.Item
+                key={genre.id}
+                onClick={() => axiosCall(genre.id, genre.name)}
+              >
+                {genre.name}
+              </Dropdown.Item>
             );
           })
         );
+
       } catch (err) {
         console.error(err);
       }
-    };
+    })();  
   });
 
-  
-
-(async function () {
-  try {
-    const options = {
-      method: "GET",
-      url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`,
-    };
-    const asyncResponse = await axios(options);
-
-    asyncResponse.data.genres.map((genre) => {
-      return genres.push(
-        <Dropdown.Item
-          key={genre.id}
-          onClick={() => axiosCall(genre.id, genre.name)}
-        >
-          {genre.name}
-        </Dropdown.Item>
+  const axiosCall = async (genreId, genreName) => {
+    try {
+      const options = {
+        method: "GET",
+        url: `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&with_genres=${genreId}&include_adult=false&include_video=false&page=1`,
+      };
+      const asyncResponse = await axios(options);
+      props.setGenre(genreName);
+      props.setMovies(
+        asyncResponse.data.results.map((movie) => {
+          return (
+            <MoviesDisplay
+              picture={movie.backdrop_path}
+              title={movie.original_title}
+              desc={movie.overview}
+              key={movie.id}
+            />
+          );
+        })
       );
-    });
-  } catch (err) {
-    console.error(err);
-  }
-})();  
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
- 
-  
     return (
       <>
         <Dropdown>
           <Dropdown.Toggle>Genre Top 20</Dropdown.Toggle>
           <Dropdown.Menu>
-            {genres}
+            {dropdowns}
          </Dropdown.Menu>
         </Dropdown>
       </>
