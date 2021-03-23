62798e5b3c2ecb4cdd265e56cb44beb073e11737
mjones12209 <m.jones12209@gmail.com>
2021-03-23 02:09:15 -0400

    fixed up some code in MoviesDisplay component

--- src/components/MoviesDisplay/MoviesDisplay.js
+++ src/components/MoviesDisplay/MoviesDisplay.js
@@ -1,54 +1,50 @@
-import React, {useState, useEffect} from 'react';
-import { Card, Button } from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
+import PropTypes from "prop-types";
 
+const MoviesDisplay = ({ overview, title, picture }) => {
+  const [text, showText] = useState(false);
+  const [desc, setDesc] = useState(null);
 
-const MoviesDisplay = ({overview, title, picture}) => {
-    const [text, showText] = useState(false);
-    const [desc, setDesc] = useState(null);
-    const showFullDesc = () => showText(!text);
+  if(overview ===  "") overview = "Sorry there was no description";
 
-    useEffect(
-        ()=> {
-           setDesc(
-            overview !== "" ? (!text ? overview.substring(0, 250) : overview) : "Sorry there was no description"
-          )
-        }
-    , [text, overview]);
-
-    return (
-      <>
-        <Card id="cardId" style={{ width: "18rem" }}>
-          {picture === null ? (
-            <Card.Img src={"https://www.hallaminternet.com/assets/show.jpg"} />
-          ) : (
-            <Card.Img src={"https://image.tmdb.org/t/p/w500/" + picture} />
-          )}
-          <Card.Body>
-            <Card.Title>
-              <strong>{title}</strong>
-            </Card.Title>
-            <Card.Text>
-              {desc}
-              <br />
-              {overview.length > 250 ? (
-                <Button onClick={showFullDesc}>
-                  {" "}
-                  Read {!text ? "More" : "Less"}
-                </Button>
-              ) : null}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </>
+  useEffect(() => {
+    setDesc(
+      !text ? overview.substring(0,0) : overview
     );
-}
+  }, [text, overview]);
+
+  return (
+    <>
+      <Card id="cardId" style={{ width: "18rem" }}>
+        {picture === null ? (
+          <Card.Img src={"https://www.hallaminternet.com/assets/show.jpg"} />
+        ) : (
+          <Card.Img src={"https://image.tmdb.org/t/p/w500/" + picture} />
+        )}
+        <Card.Body>
+          <Card.Title>
+            <strong>{title}</strong>
+          </Card.Title>
+          <Card.Text>
+            {desc}
+            <br />
+            {(
+              <Button onClick={()=>showText(!text)}>
+                 Read {!text ? "More" : "Less"}
+              </Button>
+            )}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
 MoviesDisplay.propTypes = {
-    overview: PropTypes.string,
-    title: PropTypes.string,
-    picture: PropTypes.string,
-    
-}
+  overview: PropTypes.string,
+  title: PropTypes.string,
+  picture: PropTypes.string,
+};
 
 export default MoviesDisplay;
